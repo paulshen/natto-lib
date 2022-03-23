ebee036d63fada17cded9c8e3721c7313f67dc4f
Paul Shen <paulshen0@gmail.com>
2022-03-23 15:44:44 -0700

    use named imports for atom

--- packages/eval/src/atom.ts
+++ packages/eval/src/atom.ts
@@ -1,13 +1,13 @@
-import * as mobx from "mobx";
+import { makeObservable, observable } from "mobx";
 
 export type Atom<T> = { value: T };
 export function atom<T>(initialValue: T): Atom<T> {
-  return mobx.makeObservable(
+  return makeObservable(
     {
       value: initialValue,
     },
     {
-      value: mobx.observable.ref,
+      value: observable.ref,
     }
   );
 }
